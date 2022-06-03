4d033eff1bf9774356610fc1aed8d86f1969559f
n0uben <benjamin.noureux@gmail.com>
2022-06-03 15:04:56 +0200

    ajoute includes TD5 & fonctions main hello world

--- client.c
+++ client.c
@@ -1,4 +1,18 @@
-//
-// Created by noureux2u on 03/06/2022.
-//
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
+#define PORT 6000
+#define MAX_BUFFER 1000
+#define MAX_CLIENTS 3
+#define EXIT "exit"
+
+int main(void) {
+    printf("Hello je suis le client !\n");
+}
