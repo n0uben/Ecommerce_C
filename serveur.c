0e38c58b6759484e5e86c671cc3323408c30c234
n0uben <benjamin.noureux@gmail.com>
2022-06-04 22:31:30 +0200

    ajout message coté serveur

--- serveur.c
+++ serveur.c
@@ -121,6 +121,7 @@ int main(int argc, char const *argv[])
         tampon[nbRecu] = 0;
 
         printf("LE TAMPON : %s\n", tampon);
+        printf("Lid produit : %d\n", idProduit);
 
         quantiteDemandee = atoi(tampon);
 
