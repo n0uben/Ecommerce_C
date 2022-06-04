b8f6b7b611fc9eee4ab2a37f785ed5c3e329d993
n0uben <benjamin.noureux@gmail.com>
2022-06-04 09:05:26 +0200

    ajoute code du TD5 ex1

--- client.c
+++ client.c
@@ -10,9 +10,67 @@
 
 #define PORT 6000
 #define MAX_BUFFER 1000
-#define MAX_CLIENTS 3
-#define EXIT "exit"
+#define IP_SERVER "127.0.0.1"
 
-int main(void) {
-    printf("Hello je suis le client !\n");
+int ouvrirUneConnexionTcp();
+
+int main(void)
+{
+    int fdSocket;
+    int nbRecu;
+    char tampon[MAX_BUFFER];
+
+    fdSocket = ouvrirUneConnexionTcp();
+
+    printf("Envoi du message au serveur.\n");
+    strcpy(tampon, "Message du client vers le serveur");
+    send(fdSocket, tampon, strlen(tampon), 0);
+
+    nbRecu = recv(fdSocket, tampon, MAX_BUFFER, 0); // on attend la réponse du serveur
+
+    if (nbRecu > 0) {
+        tampon[nbRecu] = 0;
+        printf("Recu : %s\n", tampon);
+    }
+
+    close(fdSocket);
+
+    return EXIT_SUCCESS;
+}
+
+int ouvrirUneConnexionTcp()
+{
+    int socketTemp;
+    int longueurAdresse;
+    struct sockaddr_in coordonneesServeur;
+
+    socketTemp = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (socket < 0)
+    {
+        printf("socket incorrecte\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // On prépare les coordonnées du serveur
+    longueurAdresse = sizeof(struct sockaddr_in);
+    memset(&coordonneesServeur, 0x00, longueurAdresse);
+
+    // connexion de type TCP
+    coordonneesServeur.sin_family = PF_INET;
+    // adresse du serveur
+    inet_aton(IP_SERVER, &coordonneesServeur.sin_addr);
+    // le port d'écoute du serveur
+    coordonneesServeur.sin_port = htons(PORT);
+
+    int connection = connect(socketTemp, (struct sockaddr *) &coordonneesServeur, sizeof(coordonneesServeur));
+    if (connection == -1)
+    {
+        printf("connexion impossible\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("connexion ok\n");
+
+    return socketTemp;
 }
--- serveur.c
+++ serveur.c
@@ -1,18 +1,102 @@
-#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 #define PORT 6000
 #define MAX_BUFFER 1000
-#define MAX_CLIENTS 3
-#define EXIT "exit"
 
-int main(void) {
-    printf("Hello je suis le serveur !\n");
+int ouvrirUneSocketAttente();
+
+int main(int argc, char const *argv[])
+{
+    int fdSocketAttente;
+    int fdSocketCommunication;
+    struct sockaddr_in coordonneesAppelant;
+    char tampon[MAX_BUFFER];
+    int nbRecu;
+
+    fdSocketAttente = ouvrirUneSocketAttente();
+
+    socklen_t tailleCoord = sizeof(coordonneesAppelant);
+
+    //on accepte la communication
+    fdSocketCommunication = accept(fdSocketAttente, (struct sockaddr *) &coordonneesAppelant, &tailleCoord);
+    if (fdSocketCommunication == -1)
+    {
+        printf("erreur de accept\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client connecté\n");
+
+    // on attend le message du client
+    // la fonction recv est bloquante
+    nbRecu = recv(fdSocketCommunication, tampon, MAX_BUFFER, 0);
+
+    if (nbRecu > 0)
+    {
+        tampon[nbRecu] = 0;
+        printf("Recu : %s\n", tampon);
+    }
+
+    printf("Envoi du message au client.\n");
+    strcpy(tampon, "Message renvoyé par le serveur vers le client !");
+    // on envoie le message au client
+    send(fdSocketCommunication, tampon, strlen(tampon), 0);
+
+    close(fdSocketCommunication);
+    close(fdSocketAttente);
+
+    return EXIT_SUCCESS;
+}
+
+
+int ouvrirUneSocketAttente()
+{
+    int socketTemp;
+    int longueurAdresse;
+    struct sockaddr_in coordonneesServeur;
+
+    socketTemp = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (socketTemp < 0)
+    {
+        printf("socket incorrecte\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // On prépare l’adresse d’attachement locale
+    longueurAdresse = sizeof(struct sockaddr_in);
+    memset(&coordonneesServeur, 0x00, longueurAdresse);
+
+    // connexion de type TCP
+    coordonneesServeur.sin_family = PF_INET;
+    // toutes les interfaces locales disponibles
+    coordonneesServeur.sin_addr.s_addr = htonl(INADDR_ANY);
+    // le port d'écoute
+    coordonneesServeur.sin_port = htons(PORT);
+
+    //on initialise la socket sur la carte réseau
+    int socketBind = bind(socketTemp, (struct sockaddr *) &coordonneesServeur, sizeof(coordonneesServeur));
+    if (socketBind == -1)
+    {
+        printf("erreur de bind\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //on lance l'ecoute sur la socket
+    int socketListen = listen(socketTemp, 5);
+    if (socketListen == -1)
+    {
+        printf("erreur de listen\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("En attente de connexion...\n");
+
+    return socketTemp;
 }
