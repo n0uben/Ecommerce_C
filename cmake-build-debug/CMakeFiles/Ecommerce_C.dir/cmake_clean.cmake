5702849e37e712c8b6cb1fe3b3b0f1bf083b2838
DrYruo <oury.jordan@hotmail.com>
2022-06-05 15:14:32 +0200

    Let's go on test !

---
+++ cmake-build-debug/CMakeFiles/Ecommerce_C.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ecommerce_C.dir/client.c.o"
+  "CMakeFiles/Ecommerce_C.dir/serveur.c.o"
+  "Ecommerce_C"
+  "Ecommerce_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ecommerce_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- serveur.c
+++ serveur.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <pthread.h>
 
 // ------------------------- Define
 #define PORT 6000
@@ -15,56 +16,143 @@
 #define NB_ARTICLES 3
 #define EXIT "exit"
 
-
-// ------------------------- Prototypes fonctions
-void initialiserMagasin(void);
-
-int getStockParArticle(int idArticle);
-
-int getPrixParArticle(int idArticle);
-
-char *getLibeleParArticle(int idArticle);
-
-void afficherArticlesEtPrix(void);
-
-void afficherStockParArticle(int idArticle);
-
-int isIdProduitValide(int idProduit);
-
-int isQuantiteDisponible(int quantitedemandee, int idProduit);
-
-int ouvrirUneSocketAttente(void);
-
-void creerFacture(int idProduit, int quantite, char facture[]);
-
-// ------------------------- Structure MAGASIN ----------------------
-struct {
+typedef struct
+{
     int idArticles[NB_ARTICLES];
     int stockParArticle[NB_ARTICLES];
     int prixParArticle[NB_ARTICLES];
+    int compteurNombreClient;
+    int fdSocketCommunication;
 } Magasin;
+
+// ------------------------- Prototypes fonctions
+void initialiserMagasin(Magasin *magasin);
+
+void *accueillirClient(void *arg);
+
+int getStockParArticle(Magasin *magasin, int idArticle);
+
+int getPrixParArticle(Magasin *magasin, int idArticle);
+
+char *getLibeleParArticle(int idArticle);
+
+//void afficherArticlesEtPrix(void);
+
+//void afficherStockParArticle(int idArticle);
+
+int isIdProduitValide(Magasin *magasin, int idProduit);
+
+int isQuantiteDisponible(Magasin *magasin, int quantitedemandee, int idProduit);
+
+int ouvrirUneSocketAttente(void);
+
+void creerFacture(Magasin *magasin, int idProduit, int quantite, char facture[]);
+
+// ------------------------- Structure MAGASIN ----------------------
+
 
 // --------------------- ---- -------------------------------------
 // --------------------- MAIN -------------------------------------
 // --------------------- ---- -------------------------------------
-int main(int argc, char const *argv[]) {
-    int fdSocketAttente;
-    int fdSocketCommunication;
-    struct sockaddr_in coordonneesAppelant;
-    char tampon[MAX_BUFFER];
-    int nbRecu;
+int main(void)
+{
 
-    initialiserMagasin();
+    Magasin magasin;
+    initialiserMagasin(&magasin);
+
+    int fdSocketAttente;
+    int *fdSocketCommunication = &magasin.fdSocketCommunication;
+    struct sockaddr_in coordonneesAppelant;
+
+    pthread_mutex_t mutex;
+    pthread_mutex_t mutexCompteur;
+    //    pthread_cond_t conditionStockOk;
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&mutexCompteur, NULL);
+
+//    pthread_t vendeur;
+    pthread_t clients[MAX_CLIENTS];
+
 
     fdSocketAttente = ouvrirUneSocketAttente();
 
     socklen_t tailleCoord = sizeof(coordonneesAppelant);
 
-    if ((fdSocketCommunication = accept(fdSocketAttente, (struct sockaddr *) &coordonneesAppelant,
-                                        &tailleCoord)) == -1) {
-        printf("erreur de accept\n");
-        exit(EXIT_FAILURE);
+    while (magasin.compteurNombreClient < MAX_CLIENTS)
+    {
+        if ((*fdSocketCommunication = accept(fdSocketAttente, (struct sockaddr *) &coordonneesAppelant,
+                                             &tailleCoord)) == -1)
+        {
+            printf("erreur de accept");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pthread_create(&clients[magasin.compteurNombreClient], NULL, accueillirClient, &magasin) != 0)
+        {
+            printf("erreur de creation de thread vendeur\n");
+            return EXIT_FAILURE;
+        }
     }
+
+
+    close(*fdSocketCommunication);
+    close(fdSocketAttente);
+
+    return EXIT_SUCCESS;
+}
+
+// ------------------------- Fonctions Magasin ----------------------
+
+
+void initialiserMagasin(Magasin *magasin)
+{
+    for (int i = 0; i < NB_ARTICLES; ++i)
+    {
+        magasin->idArticles[i] = i + 1;
+        magasin->stockParArticle[i] = 10;
+        magasin->prixParArticle[i] = 300 + ((i * 10) * 50);
+        magasin->compteurNombreClient = 0;
+    }
+    printf("Magasin initialisé !\n");
+}
+
+int getStockParArticle(Magasin *magasin, int idArticle)
+{
+    int stockArticle = magasin->stockParArticle[idArticle - 1];
+    return stockArticle;
+}
+
+int getPrixParArticle(Magasin *magasin, int idArticle)
+{
+    int prixArticle = magasin->prixParArticle[idArticle - 1];
+    return prixArticle;
+}
+
+char *getLibeleParArticle(int idArticle)
+{
+    if (idArticle == 1)
+    {
+        return "Projet en C";
+    } else if (idArticle == 2)
+    {
+        return "Projet en Java";
+    } else if (idArticle == 3)
+    {
+        return "Projet en CSS only (HTML fourni dans un pack additionnel)";
+    }
+}
+
+//--------------------------------------------------
+
+void *accueillirClient(void *arg)
+{
+    Magasin *magasin = (Magasin *) arg;
+    int fdSocketCommunication;
+    char tampon[MAX_BUFFER];
+    int nbRecu;
+
+    magasin->compteurNombreClient += 1;
 
     printf("Client connecté\n");
     printf("Envoi du catalogue au client.\n");
@@ -74,48 +162,54 @@ int main(int argc, char const *argv[]) {
     sprintf(tampon, "[%d] %s - %d€\n"
                     "[%d] %s - %d€\n"
                     "[%d] %s - %d€\n",
-            1, getLibeleParArticle(1), getPrixParArticle(1),
-            2, getLibeleParArticle(2), getPrixParArticle(2),
-            3, getLibeleParArticle(3), getPrixParArticle(3));
+            1, getLibeleParArticle(1), getPrixParArticle(magasin, 1),
+            2, getLibeleParArticle(2), getPrixParArticle(magasin, 2),
+            3, getLibeleParArticle(3), getPrixParArticle(magasin, 3));
 
     //on envoie le catalogue au client
     send(fdSocketCommunication, tampon, strlen(tampon), 0);
 
     //TRAITEMENT DU PRODUIT DEMANDÉ PAR LUTILISATEUR
     int idProduit = -1;
-    do {
+    do
+    {
         //on attend le produit demandé par le client
         nbRecu = recv(fdSocketCommunication, tampon, MAX_BUFFER, 0);
 
         //réception  de l’id du produit demandé par le client
-        if (nbRecu <= 0) {
+        if (nbRecu <= 0)
+        {
             exit(EXIT_FAILURE);
         }
         tampon[nbRecu] = 0;
         idProduit = atoi(tampon);
 
-        if (isIdProduitValide(idProduit)) {
+        if (isIdProduitValide(magasin, idProduit))
+        {
             sprintf(tampon, "[%d] %s - %d€ - Stock : %d\n", idProduit, getLibeleParArticle(idProduit),
-                    getPrixParArticle(idProduit), getStockParArticle(idProduit));
+                    getPrixParArticle(magasin, idProduit), getStockParArticle(magasin, idProduit));
             send(fdSocketCommunication, tampon, strlen(tampon), 0);
             printf("Infos envoyées au client\n");
-        } else {
+        } else
+        {
             sprintf(tampon, "%d", -1);
             send(fdSocketCommunication, tampon, strlen(tampon), 0);
             printf("Message erreur envoyé au client\n");
         }
-    } while (isIdProduitValide(idProduit) != 1);
+    } while (isIdProduitValide(magasin, idProduit) != 1);
 
 
 
     //TRAITEMENT DE LA QUANTITE DEMANDÉE PAR L'UTILISATEUR
     int quantiteDemandee = -1;
-    do {
+    do
+    {
         // on attend la quantité demandée par le client
         nbRecu = recv(fdSocketCommunication, tampon, MAX_BUFFER, 0);
 
         //réception  de la quantité demandée par le client
-        if (nbRecu <= 0) {
+        if (nbRecu <= 0)
+        {
             exit(EXIT_FAILURE);
         }
 
@@ -125,109 +219,102 @@ int main(int argc, char const *argv[]) {
         printf("LE TAMPON : %s\n", tampon);
         printf("Lid : %d\n", idProduit);
 
-        if (isQuantiteDisponible(quantiteDemandee, idProduit)) {
+        if (isQuantiteDisponible(magasin, quantiteDemandee, idProduit))
+        {
             printf("Reçu : %s\n", tampon);
 
             char facture[MAX_BUFFER];
 
-            creerFacture(idProduit, quantiteDemandee, facture);
+            creerFacture(magasin, idProduit, quantiteDemandee, facture);
             send(fdSocketCommunication, facture, strlen(facture), 0);
-            Magasin.stockParArticle[idProduit-1] = getStockParArticle(idProduit) - quantiteDemandee;
-            printf("Quantite de %s restante : %d\n", getLibeleParArticle(idProduit) , Magasin.stockParArticle[idProduit-1]);
+            magasin->stockParArticle[idProduit - 1] = getStockParArticle(magasin, idProduit) - quantiteDemandee;
+            printf("Quantite de %s restante : %d\n", getLibeleParArticle(idProduit),
+                   magasin->stockParArticle[idProduit - 1]);
 
-        } else {
+        } else
+        {
             sprintf(tampon, "%d", -1);
             send(fdSocketCommunication, tampon, strlen(tampon), 0);
             printf("Message erreur envoyé au client\n");
         }
-    } while (isQuantiteDisponible(quantiteDemandee, idProduit) != 1);
+    } while (isQuantiteDisponible(magasin, quantiteDemandee, idProduit) != 1);
 
-
-    close(fdSocketCommunication);
-    close(fdSocketAttente);
-
-    return EXIT_SUCCESS;
+    magasin->compteurNombreClient -= 1;
+    pthread_exit(NULL);
 }
 
-// ------------------------- Fonctions Magasin ----------------------
 
+//void afficherArticlesEtPrix(void)
+//{
+//    printf("[%d] %s - %d€ \n"
+//           "[%d] %s - %d€\n"
+//           "[%d] %s - %d€\n",
+//           1, getLibeleParArticle(1), getPrixParArticle(1),
+//           2, getLibeleParArticle(2), getPrixParArticle(2),
+//           3, getLibeleParArticle(3), getPrixParArticle(3));
+//}
+//
+//void afficherStockParArticle(int idArticle)
+//{
+//    if (idArticle == 1)
+//    {
+//        printf("[%d] %s - %d quantite en stock !\n", idArticle, getLibeleParArticle(idArticle),
+//               getStockParArticle(idArticle));
+//    } else if (idArticle == 2)
+//    {
+//        printf("[%d] %s - %d quantite en stock !\n", idArticle, getLibeleParArticle(idArticle),
+//               getStockParArticle(idArticle));
+//    } else if (idArticle == 3)
+//    {
+//        printf("[%d] %s - %d quantite en stock !\n", idArticle, getLibeleParArticle(idArticle),
+//               getStockParArticle(idArticle));
+//    }
+//}
 
-void initialiserMagasin(void) {
-    for (int i = 0; i < NB_ARTICLES; ++i) {
-        Magasin.idArticles[i] = i + 1;
-        Magasin.stockParArticle[i] = 10;
-        Magasin.prixParArticle[i] = 300 + ((i * 10) * 50);
-    }
-    printf("Magasin initialisé !\n");
-}
-
-int getStockParArticle(int idArticle) {
-    int stockArticle = Magasin.stockParArticle[idArticle - 1];
-    return stockArticle;
-}
-
-int getPrixParArticle(int idArticle) {
-    int prixArticle = Magasin.prixParArticle[idArticle - 1];
-    return prixArticle;
-}
-
-char *getLibeleParArticle(int idArticle) {
-    if (idArticle == 1) {
-        return "Projet en C";
-    } else if (idArticle == 2) {
-        return "Projet en Java";
-    } else if (idArticle == 3) {
-        return "Projet en CSS only (HTML fourni dans un pack additionnel)";
-    }
-}
-
-void afficherArticlesEtPrix(void) {
-    printf("[%d] %s - %d€ \n"
-           "[%d] %s - %d€\n"
-           "[%d] %s - %d€\n",
-           1, getLibeleParArticle(1), getPrixParArticle(1),
-           2, getLibeleParArticle(2), getPrixParArticle(2),
-           3, getLibeleParArticle(3), getPrixParArticle(3));
-}
-
-void afficherStockParArticle(int idArticle) {
-    if (idArticle == 1) {
-        printf("[%d] %s - %d quantite en stock !\n", idArticle, getLibeleParArticle(idArticle),
-               getStockParArticle(idArticle));
-    } else if (idArticle == 2) {
-        printf("[%d] %s - %d quantite en stock !\n", idArticle, getLibeleParArticle(idArticle),
-               getStockParArticle(idArticle));
-    } else if (idArticle == 3) {
-        printf("[%d] %s - %d quantite en stock !\n", idArticle, getLibeleParArticle(idArticle),
-               getStockParArticle(idArticle));
-    }
-}
-
-int isIdProduitValide(int idProduit) {
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (idProduit == Magasin.idArticles[i]) {
+int isIdProduitValide(Magasin *magasin, int idProduit)
+{
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        if (idProduit == magasin->idArticles[i])
+        {
             return 1;
         }
     }
     return 0;
 }
 
-int isQuantiteDisponible(int quantitedemandee, int idProduit) {
-    if (getStockParArticle(idProduit) < quantitedemandee || quantitedemandee == 0) {
+int isQuantiteDisponible(Magasin *magasin, int quantitedemandee, int idProduit)
+{
+    if (getStockParArticle(magasin, idProduit) < quantitedemandee || quantitedemandee == 0)
+    {
         return 0;
     }
     return 1;
 }
 
+void creerFacture(Magasin *magasin, int idProduit, int quantite, char facture[])
+{
+    int prixTotal = getPrixParArticle(magasin, idProduit) * quantite;
+    sprintf(facture, "Facture client\n"
+                     "- Article choisi : %s \n"
+                     "- Quantite achetée : %d - Prix unitaire : %d€ \n"
+                     "- Prix Total : %d€\n",
+            getLibeleParArticle(idProduit), quantite, getPrixParArticle(magasin, idProduit),
+            prixTotal);
+    facture[strlen(facture)] = 0;
+}
+
 // ------------------------- TCP ----------------------------------
-int ouvrirUneSocketAttente(void) {
+int ouvrirUneSocketAttente(void)
+{
     int socketTemp;
     int longueurAdresse;
     struct sockaddr_in coordonneesServeur;
 
     socketTemp = socket(PF_INET, SOCK_STREAM, 0);
 
-    if (socketTemp < 0) {
+    if (socketTemp < 0)
+    {
         printf("socket incorrecte\n");
         exit(EXIT_FAILURE);
     }
@@ -243,12 +330,14 @@ int ouvrirUneSocketAttente(void) {
     // le port d'écoute
     coordonneesServeur.sin_port = htons(PORT);
 
-    if (bind(socketTemp, (struct sockaddr *) &coordonneesServeur, sizeof(coordonneesServeur)) == -1) {
+    if (bind(socketTemp, (struct sockaddr *) &coordonneesServeur, sizeof(coordonneesServeur)) == -1)
+    {
         printf("erreur de bind\n");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(socketTemp, 5) == -1) {
+    if (listen(socketTemp, 3) == -1)
+    {
         printf("erreur de listen\n");
         exit(EXIT_FAILURE);
     }
@@ -258,13 +347,3 @@ int ouvrirUneSocketAttente(void) {
     return socketTemp;
 }
 
-void creerFacture(int idProduit, int quantite, char facture[]) {
-    int prixTotal = getPrixParArticle(idProduit) * quantite;
-    sprintf(facture, "Facture client\n"
-                     "- Article choisi : %s \n"
-                     "- Quantite achetée : %d - Prix unitaire : %d€ \n"
-                     "- Prix Total : %d€\n",
-            getLibeleParArticle(idProduit), quantite, getPrixParArticle(idProduit),
-            prixTotal);
-    facture[strlen(facture)] = 0;
-}
